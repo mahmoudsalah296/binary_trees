5806d5bfa70b88911f5ede276bf76c632edaa764
Mahmoud Salah <ms685116@gmail.com>
2023-10-30 14:04:54 +0200

    function to create a node

--- binary_trees.h
+++ binary_trees.h
@@ -1,6 +1,9 @@
 #ifndef BINARY_TREES_H
 #define BINARY_TREES_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -22,4 +25,5 @@ typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
 
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 #endif /* BINARY_TREES_H */
