8d6a45eb7d374a7da413655100fb4e35d2ca2870
Mahmoud Salah <ms685116@gmail.com>
2023-10-31 10:32:11 +0200

    add check for func

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -10,7 +10,8 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree == NULL)
 		return;
-	func(tree->n);
+	if (func)
+		func(tree->n);
 	if (tree->left)
 		binary_tree_preorder(tree->left, func);
 
