c6bc6e8c7627f6abe7bc2b0569c7268813218362
Mahmoud Salah <ms685116@gmail.com>
2023-10-31 10:34:12 +0200

    add check for func

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_postorder - goes through a binary tree using post-order traversal
+ * binary_tree_postorder - goes through a binary tree using postorder traversal
  * @tree: a pointer to the root node of the tree to traverse
  * @func:  pointer to a function to call for each node.
  *		The value in the node must be passed as a parameter to this function
@@ -16,5 +16,6 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 	if (tree->right)
 		binary_tree_postorder(tree->right, func);
 
-	func(tree->n);
+	if (func)
+		func(tree->n);
 }
