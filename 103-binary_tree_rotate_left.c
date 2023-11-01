9f71d6e9ef6002e5507e1f532dcf3fc04bbed384
Mahmoud Salah <ms685116@gmail.com>
2023-11-01 14:12:58 +0200

    rotate binary tree left

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -7,17 +7,17 @@
  */
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {
-    if (tree)
-    {
-        binary_tree_t *new_root, *tmp;
+	if (tree)
+	{
+		binary_tree_t *new_root, *tmp;
 
-        new_root = tree->right;
-        tmp = new_root->left;
-        new_root->parent = tree->parent;
-        new_root->left = tree;
-        tree->right = tmp;
-        tree->parent = new_root;
-        return (new_root);
-    }
-    return (NULL);
+		new_root = tree->right;
+		tmp = new_root->left;
+		new_root->parent = tree->parent;
+		new_root->left = tree;
+		tree->right = tmp;
+		tree->parent = new_root;
+		return (new_root);
+	}
+	return (NULL);
 }
